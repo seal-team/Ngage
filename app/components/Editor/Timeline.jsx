3f4662454351083ccd8076097310977eae46e805
Alvin Wen <alvinwen424@gmail.com>
2017-08-14 16:01:42 -0400

    fixes timeline

--- app/components/Editor/Timeline.jsx
+++ app/components/Editor/Timeline.jsx
@@ -1,8 +1,46 @@
 import React, { Component } from 'react'
+import AddSlides from '../AddSlides'
 
 class Timeline extends Component {
-  constructor(props) {
-    super(props)
+  constructor() {
+    super()
+    this.state = {
+      slides: [],
+      slidesCount: 0,
+      uid: null
+    }
+  }
+  componentDidMount(){
+    firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+            this.setState({uid: user.uid})
+            const activePresentation = firebase.database()
+                .ref('users')
+                .child(user.uid)
+                .child('activePresentation')
+            // activePresentation.on('value', (snapshot) => {
+            //   let items = snapshot.val()
+            // })
+          } else {
+            this.setState({uid: null})
+      }
+      })
+  }
+
+  handleSubmit = (e) => {
+      e.preventDefault()
+      const activePresentation = firebase.database()
+        .ref('users')
+        .child(this.state.uid)
+        .child('activePresentation')
+      const slides = firebase.database()
+        .ref('presentation')
+        .child(activePresentation)
+        .child('slides')
+
+      this.setState({ slidesCount: this.state.slidesCount++ })
+      
+      slides.child(this.state.slidesCount).set({})
   }
 
   render() {
@@ -20,7 +58,8 @@ class Timeline extends Component {
           <div className="timeline-slide"></div>
           <div className="timeline-slide"></div>
 
-          <div className="plus-slide-btn">
+          <div className="plus-slide-btn"
+            onClick={this.handleSubmit}>
             <span className="icon">
               <i className="fa fa-plus-square-o"></i>
             </span>
