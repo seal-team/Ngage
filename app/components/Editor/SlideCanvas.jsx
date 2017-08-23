7a5c0213921a166d0dac9673ba408b2ef55750a2
Stacy Hirschberg <stacyhi511@gmail.com>
2017-08-22 22:59:49 -0400

    fix: vr slideCanvas listener

--- app/components/Editor/SlideCanvas.jsx
+++ app/components/Editor/SlideCanvas.jsx
@@ -18,9 +18,10 @@ class SlideCanvas extends Component {
   componentDidMount() {
     const { presentationID, slideID } = this.props.match.params
     firebase.database()
-      .ref(`presentations/${presentationID}/slides/${slideID}`)
-      .once('value', snapshot => {
-        this.setState({ info: snapshot.val() })
+      .ref(`presentations/${presentationID}/slides/`)
+      .on('value', snapshot => {
+        console.log('didmount', snapshot.val()[slideID])
+        this.setState({ info: snapshot.val()[slideID] })
       })
   }
 
@@ -28,11 +29,11 @@ class SlideCanvas extends Component {
     const { presentationID, slideID } = this.props.match.params
     const info = this.state.info
     const slideType = getSlideType(presentationID, slideID)
-
+    console.log('render info', this.state.info)
     return (
       <div className="slide-canvas-container">
         { slideType === 'quill' && <QuillComp /> }
-        { slideType === 'VR' && <VRComp 
+        { slideType === 'VR' && this.state.info.VRContents && <VRComp
           obj={this.state.info.VRContents.VRurl[0]}
           mtl={this.state.info.VRContents.VRurl[1]}
           description= {this.state.info.VRContents.description}/>}
